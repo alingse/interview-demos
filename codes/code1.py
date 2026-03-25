2fc76ecb6eb3d971184b7fc02283e1ce428e5101
alingse <alingse@foxmail.com>
2026-03-25 17:03:13 +0800

    feat: add degree of array algorithm solution

---
+++ codes/code1.py
@@ -0,0 +1,60 @@
+'''
+给定一个非空且只包含非负数的整数数组 nums，数组的 度 的定义是指数组里任一元素出现频数的最大值。
+
+你的任务是在 nums 中找到与 nums 拥有相同大小的度的最短连续子数组，返回其长度。
+示例 1：
+
+输入：nums = [1,2,2,3,1]
+输出：2
+解释：
+输入数组的度是 2 ，因为元素 1 和 2 的出现频数最大，均为 2 。
+连续子数组里面拥有相同度的有如下所示：
+[1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+最短连续子数组 [2, 2] 的长度为 2 ，所以返回 2 。
+示例 2：
+
+输入：nums = [1,2,2,3,1,4,2]
+输出：6
+解释：
+数组的度是 3 ，因为元素 2 重复出现 3 次。
+所以 [2,2,3,1,4,2] 是最短子数组，因此返回 6 。
+
+提示：
+nums.length 在 1 到 50,000 范围内。
+nums[i] 是一个在 0 到 49,999 范围内的整数。
+'''
+
+
+def degree(nums):
+    m = dict()
+    start_end_m = dict()
+
+    max_value = 0
+    max_value_sub = 0
+    for i in range(len(nums)):
+        n = nums[i]
+        if n not in m:
+            start_end_m[n] = (i, i)
+            v = 1
+            m[n] = v
+        else:
+            v = m[n] + 1
+            m[n] = v
+            start, _ = start_end_m[n]
+            start_end_m[n] = (start, i)
+
+        start, end = start_end_m[n]
+        current_sub = end - start +1
+        if v > max_value:
+            max_value = v
+            max_value_sub = current_sub
+        elif v == max_value:
+            # keep small
+            max_value_sub = min(max_value_sub, current_sub)
+
+    return max_value_sub
+
+if __name__ == "__main__":
+    print(degree([1,2,2,3,1]))
+    print(degree([1,2,2,3,1,4,2]))
+
